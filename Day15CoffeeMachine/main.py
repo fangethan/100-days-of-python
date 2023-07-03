2dccb63955a6e33f349ad1f6fc07380947b8629b
Ethan Fang <ethan.fang@gmail.com>
2023-07-04 00:40:37 +1000

    Coffee machine can ask user what they like and print report

--- Day15CoffeeMachine/main.py
+++ Day15CoffeeMachine/main.py
@@ -29,3 +29,53 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+total_profit = 0
+
+def printReport():
+    print("Resources remaining:")
+    print("Water: " + str(resources["water"]) + "ml")
+    print("Milk: " + str(resources["milk"]) + "ml")
+    print("Coffee: " + str(resources["coffee"]) + "g")
+    print("Money: " + "$" + str(total_profit))
+
+def are_resources_sufficient(drink_of_choice):
+    global total_profit
+    if drink_of_choice == "espresso":
+        if resources["water"] >= MENU["espresso"]["ingredients"]["water"] and resources["coffee"] >= MENU["espresso"]["ingredients"]["coffee"]:
+            
+            resources["water"] -= MENU["espresso"]["ingredients"]["water"]
+            resources["coffee"] -= MENU["espresso"]["ingredients"]["coffee"]
+            total_profit += MENU["espresso"]["cost"]
+    
+    elif drink_of_choice == "latte":
+        if (resources["water"] >= MENU["latte"]["ingredients"]["water"] and resources["milk"] >= MENU["latte"]["ingredients"]["milk"] and
+            resources["coffee"] >= MENU["latte"]["ingredients"]["coffee"]):
+            
+            resources["water"] -= MENU["latte"]["ingredients"]["water"]
+            resources["coffee"] -= MENU["latte"]["ingredients"]["coffee"]
+            resources["milk"] -= MENU["latte"]["ingredients"]["milk"]
+            total_profit += MENU["latte"]["cost"]
+    else:
+        if (resources["water"] >= MENU["cappuccino"]["ingredients"]["water"] and resources["milk"] >= MENU["cappuccino"]["ingredients"]["milk"] and
+            resources["coffee"] >= MENU["cappuccino"]["ingredients"]["coffee"]):
+            
+            resources["water"] -= MENU["cappuccino"]["ingredients"]["water"]
+            resources["coffee"] -= MENU["cappuccino"]["ingredients"]["coffee"]
+            resources["milk"] -= MENU["cappuccino"]["ingredients"]["milk"]
+            total_profit += MENU["cappuccino"]["cost"]
+
+is_coffee_machine_on = True
+
+while is_coffee_machine_on:
+    choice = input("Hello customer! What would you like? (espresso/latte/cappuccino): ")
+
+    if choice == "off":
+        is_coffee_machine_on = False
+    elif choice == "report":
+        printReport()
+    elif choice.lower() in MENU:
+        print(choice)
+        are_resources_sufficient(choice.lower())
+
+
