8ed8790971618ed2700c73c92bbbec8210da80c8
Ethan Fang <ethan.fang@gmail.com>
2023-07-07 00:08:17 +1000

    Create QuizBrain class and its method

--- Day17QuizGame/main.py
+++ Day17QuizGame/main.py
@@ -1,10 +1,11 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for question in question_data:
     question_bank.append(Question(question["text"], question["answer"]))
-    print(question)
 
-print(question_bank)
+quiz_brain = QuizBrain(question_bank)
+quiz_brain.next_question()
