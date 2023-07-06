5bfba0f9dee427db62292c47ac40d797303d0284
Ethan Fang <ethan.fang@gmail.com>
2023-07-07 00:27:21 +1000

    Add final component to end quiz

--- Day17QuizGame/main.py
+++ Day17QuizGame/main.py
@@ -11,3 +11,6 @@ quiz_brain = QuizBrain(question_bank)
 
 while quiz_brain.still_has_questions(): 
     quiz_brain.next_question()
+
+print("You have completed the quiz!")
+print(f"Your final score is: {quiz_brain.score}/{len(question_bank)}")
