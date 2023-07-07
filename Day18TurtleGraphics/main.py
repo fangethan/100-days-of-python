179e22ef0073c86d56e71cac21b24b117733e14e
Ethan Fang <ethan.fang@gmail.com>
2023-07-07 17:54:41 +1000

    Draw multiple shapes consecutively

--- Day18TurtleGraphics/main.py
+++ Day18TurtleGraphics/main.py
@@ -13,15 +13,28 @@ tim.color("red")
 
 
 ######## Challenge 1 - Draw a Square ############
-for _ in range(4):
-    tim.forward(100)
-    tim.left(90)
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.left(90)
 
 
 ######## Challenge 2 - Draw a Dashed Line ############
-for _ in range (10):
-    tim.color("blue")
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+# for _ in range (10):
+#     tim.color("blue")
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
+
+######## Challenge 3 - Drawing different shapes ############
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shapes_sides in range(3,11):
+    draw_shape(shapes_sides)
+
