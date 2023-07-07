712a9027b0f0a1ed5ea85a5e0091ebc23612c04f
Ethan Fang <ethan.fang@gmail.com>
2023-07-07 18:10:33 +1000

    Can generate random walk with random colours

--- Day18TurtleGraphics/main.py
+++ Day18TurtleGraphics/main.py
@@ -1,10 +1,12 @@
 #####Turtle Intro######
 
 import turtle as t
+import random
 
 tim = t.Turtle()
 tim.shape("turtle")
 tim.color("red")
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 # tim.forward(100)
 # tim.backward(200)
 # tim.right(90)
@@ -29,12 +31,31 @@ tim.color("red")
 
 ######## Challenge 3 - Drawing different shapes ############
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
 
-for shapes_sides in range(3,11):
-    draw_shape(shapes_sides)
+# for shapes_sides in range(3,11):
+#     draw_shape(shapes_sides)
+
+######## Challenge 4 - Generate a random walk ############
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+t.colormode(255)
+
+def random_colour():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    colour = (r, g, b)
+    return colour
+
+for _ in range(200):
+    # tim.color(random.choice(colours))
+    tim.color(random_colour())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 
