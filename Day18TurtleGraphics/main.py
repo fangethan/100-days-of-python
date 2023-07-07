f1b57da541acbeb934849b22356281ce1a7fb589
Ethan Fang <ethan.fang@gmail.com>
2023-07-07 17:41:42 +1000

    Can draw a square:

--- Day18TurtleGraphics/main.py
+++ Day18TurtleGraphics/main.py
@@ -2,14 +2,18 @@
 
 import turtle as t
 
-timmy_the_turtle = t.Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.backward(200)
-timmy_the_turtle.right(90)
-timmy_the_turtle.left(180)
-timmy_the_turtle.setheading(0)
+tim = t.Turtle()
+tim.shape("turtle")
+tim.color("red")
+# tim.forward(100)
+# tim.backward(200)
+# tim.right(90)
+# tim.left(180)
+# tim.setheading(0)
 
 
 ######## Challenge 1 - Draw a Square ############
+for _ in range(4):
+    tim.forward(100)
+    tim.left(90)
+
