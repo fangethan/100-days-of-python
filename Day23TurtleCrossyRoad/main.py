5f6e4a0a6c3c0133b4edd769e2685ebc55ee32ed
Ethan Fang <ethan.fang@gmail.com>
2023-07-18 16:54:29 +1000

    Game ends if turtle hits car

--- Day23TurtleCrossyRoad/main.py
+++ Day23TurtleCrossyRoad/main.py
@@ -19,5 +19,11 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
     car_manager.create_car()
     car_manager.move_car()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            print("Game over")
+            game_is_on = False
