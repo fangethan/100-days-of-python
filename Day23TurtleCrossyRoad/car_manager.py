b348de7dab2cb66f7181c3fee48c379ee48e425f
Ethan Fang <ethan.fang@gmail.com>
2023-07-18 16:51:53 +1000

    Add car behaviours

--- Day23TurtleCrossyRoad/car_manager.py
+++ Day23TurtleCrossyRoad/car_manager.py
@@ -1,7 +1,26 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self) -> None:
+        self.all_cars = []
+
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 6:
+            new_car = Turtle("square")
+            new_car.shapesize(1, 2)
+            new_car.penup()
+            new_car.speed("normal")
+            new_car.color(random.choice(COLORS))
+            new_car.goto(300, random.randint(-250, 250))
+            self.all_cars.append(new_car)
+
+    def move_car(self):
+        for car in self.all_cars:
+            car.backward(MOVE_INCREMENT)
