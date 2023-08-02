89b9b9024cf8eb3d285ebeca265e25c3279181f8
Ethan Fang <ethan.fang@gmail.com>
2023-08-02 23:28:30 +1000

    Complete guess number

--- Day55GuessTheNumber/main.py
+++ Day55GuessTheNumber/main.py
@@ -1,19 +1,38 @@
 from flask import Flask
+import random
+
+random_number = random.randint(0, 9)
+print(random_number)
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return (
+        '<h1>Guess a number between 0 and 9" and display a gif of your choice from giphy.com.</h1>'
+        '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif"></img>'
+    )
 
-@app.route("/username/<name>")
-def greet(name):
-    return f"Hello there {name}"
 
-@app.route("/bye")
-def say_bye():
-    return "Bye"
+@app.route("/url/<int: number>")
+def guess_number(number):
+    if number > random_number:
+        return (
+            '<h1 style="color: red">Too high!</h1>'
+            '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.g"></img>'
+        )
+    if number < random_number:
+        return (
+            '<h1 style="color: red">Too low!</h1>'
+            '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif"></img>'
+        )
+
+    return (
+        '<h1 style="color: green">You found me!</h1>'
+        '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif"></img>'
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
