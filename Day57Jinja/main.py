477fb501190460c3c99895c145092e0446b1fdaf
Ethan Fang <ethan.fang@gmail.com>
2023-08-03 23:56:59 +1000

    Jinja multiline statements

--- Day57Jinja/main.py
+++ Day57Jinja/main.py
@@ -3,9 +3,11 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html", name="Katie")
+
 
 @app.route("/guess/<name>")
 def guess(name):
@@ -17,7 +19,18 @@ def guess(name):
     age_response = requests.get(age_url)
     age_data = age_response.json()
 
-    return render_template("guess.html", name=name, gender=gender_data["gender"], age=age_data["age"])
+    return render_template(
+        "guess.html", name=name, gender=gender_data["gender"], age=age_data["age"]
+    )
+
+
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
