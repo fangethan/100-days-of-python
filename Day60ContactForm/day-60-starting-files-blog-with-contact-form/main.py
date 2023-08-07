125b0e41857ffd12e01ffc20e5f4ea26b833cd1c
Ethan Fang <ethan.fang@gmail.com>
2023-08-08 00:02:10 +1000

    Can send emails

--- Day60ContactForm/day-60-starting-files-blog-with-contact-form/main.py
+++ Day60ContactForm/day-60-starting-files-blog-with-contact-form/main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
+from password import PASSWORD
 
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
@@ -24,9 +26,20 @@ def contact():
         print(data["email"])
         print(data["phone"])
         print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
-    
+
     return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login("ethan.fang@gmail.com", PASSWORD)
+        connection.sendmail(
+            "ethan.fang@gmail.com", "ethan.fang@gmail.com", email_message
+        )
 
 
 @app.route("/post/<int:index>")
@@ -36,6 +49,7 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
