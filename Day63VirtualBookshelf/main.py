e6b68ea8e6789f6d117edf82fd1c9a8bdd93bc05
Ethan Fang <ethan.fang@gmail.com>
2023-08-13 15:47:25 +1000

    Can edit ratings

--- Day63VirtualBookshelf/main.py
+++ Day63VirtualBookshelf/main.py
@@ -51,6 +51,18 @@ def add():
 
     return render_template("add.html")
 
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit.html", book=book_selected)
 
 if __name__ == "__main__":
     app.run(debug=True)
