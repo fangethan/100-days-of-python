6dae9261ff0f5ee8fce20337967a4b1fb497bec5
Ethan Fang <ethan.fang@gmail.com>
2023-08-16 19:06:25 +1000

    Get all cafes

--- Day66RestApi/main.py
+++ Day66RestApi/main.py
@@ -37,6 +37,8 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 with app.app_context():
     db.create_all()
@@ -52,19 +54,16 @@ def get_random_cafe():
     all_cafes = result.scalars().all()
     random_cafe = random.choice(all_cafes)
 
-    return jsonify(cafe={
-        "id": random_cafe.id,
-        "name": random_cafe.name,
-        "map_url": random_cafe.map_url,
-        "img_url": random_cafe.img_url,
-        "location": random_cafe.location,
-        "seats": random_cafe.seats,
-        "has_toilet": random_cafe.has_toilet,
-        "has_wifi": random_cafe.has_wifi,
-        "has_sockets": random_cafe.has_sockets,
-        "can_take_calls": random_cafe.can_take_calls,
-        "coffee_price": random_cafe.coffee_price,
-    })
+    return jsonify(cafe=random_cafe.to_dict())
+
+
+@app.route("/all")
+def get_all_cafes():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
 
 ## HTTP GET - Read Record
 
