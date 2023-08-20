96721e3789ae7cd893e024fd747e2f8a51f3dbfa
Ethan Fang <ethan.fang@gmail.com>
2023-08-21 00:48:53 +1000

    Can login and logout

--- Day68AuthenticationFlask/main.py
+++ Day68AuthenticationFlask/main.py
@@ -26,9 +26,17 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 db = SQLAlchemy()
 db.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
 
 # CREATE TABLE IN DB
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
@@ -47,37 +55,57 @@ def home():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        hash_and_salted_password = generate_password_hash(
+            request.form.get("password"), method="pbkdf2:sha256", salt_length=8
+        )
         new_user = User(
             email=request.form.get("email"),
             name=request.form.get("name"),
-            password=request.form.get("password"),
+            password=hash_and_salted_password,
         )
         db.session.add(new_user)
         db.session.commit()
+
+        login_user(new_user)
+
         return render_template("secrets.html", name=request.form.get("name"))
 
     return render_template("register.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for("secrets"))
+
     return render_template("login.html")
 
 
 @app.route("/secrets")
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("secrets.html", name=current_user.name)
 
 
 @app.route("/logout")
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
+
 
 
 @app.route("/download")
+@login_required
 def download():
-    return send_from_directory('static', path="files/cheat_sheet.pdf")
-
+    return send_from_directory("static", path="files/cheat_sheet.pdf")
 
 
 if __name__ == "__main__":
