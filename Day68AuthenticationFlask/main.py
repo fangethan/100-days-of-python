0ef9b55e4e1c1d58be6f445e87120d90e4137005
Ethan Fang <ethan.fang@gmail.com>
2023-08-24 23:44:03 +1000

    Hide login and register when user is already logged in

--- Day68AuthenticationFlask/main.py
+++ Day68AuthenticationFlask/main.py
@@ -49,7 +49,7 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -77,7 +77,7 @@ def register():
 
         return render_template("secrets.html", name=request.form.get("name"))
 
-    return render_template("register.html")
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -99,13 +99,13 @@ def login():
         login_user(user)
         return redirect(url_for("secrets"))
 
-    return render_template("login.html")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/secrets")
 @login_required
 def secrets():
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route("/logout")
